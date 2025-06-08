9a77e6de153040f52601ba8d02445979b91ebf66
Brendan <brendan.q.hurt@gmail.com>
2025-06-07 17:58:02 -0700

    Got basic foundry form working
    Made the basic structure and functionality of the form for adding items to the foundry/farming page.

--- src/components/FoundryItemForm.vue
+++ src/components/FoundryItemForm.vue
@@ -1,31 +1,53 @@
-<script setup lang="ts"></script>
+<script lang="ts">
+export default {
+  data() {
+    return {
+      material: [
+        {
+          name: '',
+          amount: 0,
+        },
+      ],
+    }
+  },
+  methods: {
+    addMaterial() {
+      this.material.push({
+        name: '',
+        amount: 0,
+      })
+    },
+    deleteMaterial(counter: number) {
+      this.material.splice(counter, 1)
+    },
+  },
+}
+</script>
 
 <template>
   <form method="post">
-    <label>
-      Name:
-      <input name="submitted-name" />
-    </label>
-    <label>
-      How many you own:
-      <input type="number" />
-    </label>
-    <label>
-      Drop Chance:
-      <input type="number" />
-    </label>
-    <label>
-      Drop Source:
-      <input />
-    </label>
-    <label>
-      Materials:
-      <form>
-        <label>Name: <input /></label>
-        <label>Amount: <input type="number" /></label>
-        <button>Add another material:</button>
-      </form>
-    </label>
+    <label>Name</label><input type="text" required />
+    <br />
+    <label>How many you own</label><input type="number" />
+    <br />
+    <label>Drop Chance</label><input type="number" />
+    <br />
+    <label>Drop Source</label><input type="text" />
+    <br />
+    <fieldset>
+      <legend>Materials</legend>
+      <label for="name">Name <input /></label>
+      <label for="amount">Amount <input type="number" /></label>
+      <br />
+      <div class="matCard" v-for="(name, amount) in material" v-bind:key="counter">
+        <span @click="deleteMaterial(counter)">x </span>
+        <label for="name">Name </label>
+        <input type="text" v-model="material.name" required />
+        <label for="amount"> Amount </label>
+        <input type="number" v-model="material.amount" required />
+      </div>
+      <button @click="addMaterial">Add another material:</button>
+    </fieldset>
 
     <button>Cancel</button>
     <submit>Confirm</submit>
