71f525917ec62b97e44567b12dde0efe973c115f
Brendan <brendan.q.hurt@gmail.com>
2025-07-27 17:16:18 -0700

    Restructured Project
    Moved the frontend Vue app to its own directory rather than having it rather than having it in the root. Also, migrated from npm to yarn.

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,6 +1,5 @@
-import HomeView from '@/views/FoundryView.vue'
 import { createRouter, createWebHistory } from 'vue-router'
-//import HomeView from '../views/HomeView.vue'
+import FoundryView from '../views/FoundryView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,7 +7,7 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: HomeView,
+      component: FoundryView,
     },
   ],
 })
